f38af9ecb5e4b5f6bc701009d143ae3ecd221d0c
Your Name <you@example.com>
2026-05-26 10:01:41 +0200

    aaa

---
+++ zadania/T69a - tablica asocjacyjna/wynik.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>T69a - tablica asocjacyjna</title>
+</head>
+<body>
+
+<h1>T69a - tablica asocjacyjna</h1>
+<h2>Autor: Jakub Płudowski</h2>
+<hr>
+
+<?php
+
+    $tablica = array("Polska"=>"warszawa","niemcy"=>"berlin","francja"=>"paryż");
+    echo"<pre>";
+    print_r($tablica);
+    echo"</pre>"
+?>
+
+</body>
+</html>
